85e48b60949d8ca79b03d64c5b16fc1546cee4b4
lokihowellstudios <lokihowellstudios@gmail.com>
2024-01-11 20:27:53 -0500

    Win-Screen Update

---
+++ Assets/Winscreen.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Winscreen : MonoBehaviour
+{
+    public void GoToMenu() {  
+        SceneManager.LoadScene("Menu");  
+    } 
+        
+
+}
