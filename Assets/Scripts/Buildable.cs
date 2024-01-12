ae09be15b96ee484fd5b707e4e47f7019ad7540f
Tyler Pelaez <mail@tylerpelaez.com>
2024-01-11 20:33:18 -0500

    Starting catapult

--- Assets/Scripts/Buildable.cs
+++ Assets/Scripts/Buildable.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Buildable : MonoBehaviour
@@ -9,16 +7,9 @@ public class Buildable : MonoBehaviour
 
     [SerializeField]
     private ParticleSystem buildParticles;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        // Show UI For Building pieces missing
     }
 }
